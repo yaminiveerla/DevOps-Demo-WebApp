d6efd89bddb39b0c10a6962a01b879a57b63ede9
yaminiveerla <70117846+yaminiveerla@users.noreply.github.com>
2020-10-07 19:17:28 -0400

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://35.238.203.84:8080/QAWebapp/";
-	public String URL1 = "http://35.238.203.84:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://34.122.249.120:8080/QAWebapp/";
+	public String URL1 = "http://34.122.249.120:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
